7ef42758278298f266b71c2598cd3e8f11e53622
ripwin <sathiya.kirushnapillai@gmail.com>
2017-09-23 14:02:40 +0200

    First implementation of EJS and express

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("loool");
+const express = require('express');
+
+const app = express();
+
+
+// Configuration du moteur de vue
+app.set('view engine', 'ejs');
+
+
+// Accès à l'index
+app.get('/', function (req, res) {
+  res.render('pages/index');
+});
+
+
+// Spécifie le port d'accès au serveur
+app.listen(9090);
