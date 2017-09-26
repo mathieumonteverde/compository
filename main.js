fe0a71b566ffcec5ff70739ebeee5b2f5b0c37a6
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-09-26 17:30:00 +0200

    Update githubapi

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const githubapi = require('./githubapi.js');
 
 const app = express();
 
@@ -18,6 +19,21 @@ app.get('/landing_page', (req, res) => {
   res.render('pages/landing_page');
 });
 
+// Example d'utilisation
+app.get('/test', (req, res) => {
+  // Promise permet d'enchainer plusieurs callback en synchro
+  new Promise((resolve, reject) => {
+    // Récupère un repo
+    githubapi.getRepo('mathieumonteverde', 'compository', (response) => {
+      resolve(response);
+    });
+  }).then((data) => {
+    // Envoie les données à la vues
+    res.render('pages/test', {
+      repo: JSON.parse(data),
+    });
+  });
+});
 
 // Spécifie le port d'accès au serveur
 app.listen(9090);
