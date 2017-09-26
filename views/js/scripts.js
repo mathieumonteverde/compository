7f2e4b66eb0cc751fbcc008267c45458fe4385af
Viking & Robot <vikingandrobot@gmail.com>
2017-09-26 21:40:52 +0200

    Added repository input verification on landing_page view and fixed layout

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -1,15 +1,25 @@
-$(document).ready(function() {
-
+$(document).ready(() => {
   /* Register event to scroll to targeted elements on button click */
-  $('.scroll-action').click(function() {
-  	let idSelector = `#${$(this).attr('data-scroll-target')}`;
+  $('.scroll-action').click(function () {
+  	const idSelector = `#${$(this).attr('data-scroll-target')}`;
     scrollToTarget(idSelector);
   });
 
   /* Register event for the click on add repository button */
-  $('#add-repository').click(function() {
-    let repositoryUrl = $(this).siblings('.repository-input').val();
-    if (!(repositoryUrl === '')) {
+  $('#add-repository').click(function () {
+    // Retrieve repository url input
+    const repositoryUrl = $(this).siblings('.repository-input').val();
+
+    // Check the input, and if valid, add it to the list of repositories
+    if (repositoryUrl === '') {
+      // Show empty input repository alert
+      $('#alert-empty-repository').show(500);
+      setTimeout(() => { $('#alert-empty-repository').hide(500); }, 4000);
+    } else if ($(`.list-group-item[data-repository-url=${repositoryUrl}]`).length > 0) {
+      // Show existing repositiory input
+      $('#alert-exist-repository').show(500);
+      setTimeout(() => { $('#alert-exist-repository').hide(500); }, 4000);
+    } else {
       addRepositoryToList(repositoryUrl);
     }
   });
@@ -22,7 +32,7 @@ $(document).ready(function() {
 */
 function scrollToTarget(elementSelector) {
   $('html, body').animate({
-    scrollTop: $(elementSelector).offset().top
+    scrollTop: $(elementSelector).offset().top,
   }, 'slow');
 }
 
@@ -44,9 +54,10 @@ function toggleCompareButton() {
 */
 function addRepositoryToList(repositoryUrl) {
   const template = $('.repository-list-item-template').clone();
+  template.attr('data-repository-url', repositoryUrl);
   template.removeClass('repository-list-item-template').find('.url').html(repositoryUrl);
   $('.repository-list .list-group').append(template);
-  template.find('.control-delete').click(function() {
+  template.find('.control-delete').click(function () {
     $(this).parents('.list-group-item').remove();
     toggleCompareButton();
   });
