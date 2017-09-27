04ac6c99a3188b576d7b6f68f4e5aff5949f1cba
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-09-27 08:58:54 +0200

    Example of sychro request

--- main.js
+++ main.js
@@ -21,18 +21,67 @@ app.get('/landing_page', (req, res) => {
 
 // Example d'utilisation
 app.get('/test', (req, res) => {
-  // Promise permet d'enchainer plusieurs callback en synchro
-  new Promise((resolve, reject) => {
+  const getRepo = new Promise((resolve, reject) => {
     // Récupère un repo
     githubapi.getRepo('mathieumonteverde', 'compository', (response) => {
-      resolve(response);
-    });
-  }).then((data) => {
-    // Envoie les données à la vues
-    res.render('pages/test', {
-      repo: JSON.parse(data),
+      const json = JSON.parse(response);
+
+      const data = {};
+      data.name = json.name;
+      data.full_name = json.full_name;
+      data.description = json.description;
+      data.created_at = json.created_at;
+      data.updated_at = json.updated_at;
+      data.pushed_at = json.pushed_at;
+      data.forks_count = json.forks_count;
+      data.open_issues_count = json.open_issues_count;
+
+      resolve(data);
     });
   });
+
+  const contributors = function (d) {
+    return new Promise((resolve, reject) => {
+      // Récupère les contributeurs
+      githubapi.getContributors('mathieumonteverde', 'compository', (response) => {
+        const json = JSON.parse(response);
+
+        const data = {};
+        data.contributors_count = Object.keys(json).length;
+
+        Object.assign(data, d);
+
+        resolve(data);
+      });
+    });
+  };
+
+  const commits = function(d) {
+    return new Promise((resolve, reject) => {
+      // Récupère les commits
+      githubapi.getCommits('mathieumonteverde', 'compository', (response) => {
+        const json = JSON.parse(response);
+        const nbCommits = Object.keys(json).length;
+
+        const data = {};
+        data.commits_count = nbCommits;
+
+        Object.assign(data, d);
+
+        resolve(data);
+      });
+    });
+  };
+
+  getRepo
+    .then(contributors)
+    .then(commits)
+    .then((data) => {
+      // Envoie les données à la vues
+      res.render('pages/test', {
+        data: data,
+      });
+    });
 });
 
 // Spécifie le port d'accès au serveur
