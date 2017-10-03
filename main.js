6ae44fd3c0da302664bf4b4908899c6942644c13
Viking & Robot <vikingandrobot@gmail.com>
2017-10-03 15:09:19 +0200

    Implemented the basic information retrival from GitHub API on /compare POST requets.

--- main.js
+++ main.js
@@ -30,15 +30,77 @@ app.get('/compare', (req, res) => {
   res.render('pages/compare');
 });
 
-/* POST compare to get repositoriy data requests */
+/*
+  Process POST requests on /compare path.
+
+  The requests are supposed to provide data containing
+  information about one or more repositories.
+*/
 app.post('/compare', (req, res) => {
   // Parse Form JSON information
   const info = JSON.parse(req.body.repositories);
-  
-  // Render the view
-  res.render('pages/compare', {
-    data: info,
-  });
+
+  /*
+    Object to store main information about the repositories retrieved from
+    the GitHub API.
+  */
+  const repositoriesSummary = {
+    expectedNumber: info.repositories.length,
+    numberOfErrors: 0, // How many repositories could not be retrieved
+    list: [],
+  };
+
+  /*
+    Function to process every github api response.
+  */
+  const processGithubResponse = (response) => {
+    // Parse the response
+    const json = JSON.parse(response);
+
+    /*
+      The GitHub API response will have a 'message' property set to 'Not Found'
+      if the request could not be fullfilled...
+      Check if the request was a success, and if it's the case, add the main
+      information fields to the list of repository summaries.
+      */
+    if (json.message !== undefined && json.message === 'Not Found') {
+      // Increase the number the number of error on repository request
+      repositoriesSummary.numberOfErrors += 1;
+    } else {
+      // Add the main information to the list of repository summaries
+      repositoriesSummary.list.push({
+        name: json.name,
+        full_name: json.full_name,
+        owner: json.owner.login,
+        description: json.description,
+      });
+    }
+
+    /*
+      At the end of the request, if there is enough repository summaries in the
+      list (regarding the number of requests and the number of errors), we can
+      render the view.
+    */
+    if (repositoriesSummary.list.length === repositoriesSummary.expectedNumber
+      - repositoriesSummary.numberOfErrors) {
+      // Render the view
+      res.render('pages/compare', {
+        data: repositoriesSummary,
+      });
+    }
+  };
+
+  /*
+    For every repository submitted in the POST request, make a GitHub API
+    request to retrieve the main information.
+
+    We pass the processGithubResponse function as a callback.
+  */
+  for (let i = 0; i < info.repositories.length; i += 1) {
+    const repositoryInfos = info.repositories[i];
+
+    githubapi.getRepo(repositoryInfos.user, repositoryInfos.repository, processGithubResponse);
+  }
 });
 
 // Example d'utilisation
