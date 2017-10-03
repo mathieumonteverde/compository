6eb19763b86cd66eb72b869f99241ee59074ee8f
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-10-03 17:32:02 +0200

    Add ; for ESLint

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const githubapi = require('./githubapi.js');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
