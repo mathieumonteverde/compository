8ec467f9ca8fd10eeef771e05da734d4715d029a
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-10-22 11:23:16 +0200

    Config ESLint for mocha

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
     "extends": "airbnb-base",
     "env": {
       "jquery": true,
+      "mocha": true,
     },
     "globals" : {
       "document": false,
       "RepositorySelector": false,
     },
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
 };
